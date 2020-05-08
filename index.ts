e139146b54a8748caab3338e8cc8e8a4399490b2
Elliot Ng <chihimng.io@gmail.com>
2020-05-09 01:00:20 +0800

    feat: parse horse info

--- index.ts
+++ index.ts
@@ -18,30 +18,39 @@ async function main () {
             horseRefs.push(...await page.$$eval('li a[class="table_eng_text"]', elems => elems.map(elem => elem.getAttribute('href') ?? ''))) 
             console.log(`Found ${horseRefs.length} links`)
             await sleep(500)
-            break // for debug
+            // break // for debug
         }
         await writeFile('./output/horseRefs.json', JSON.stringify(horseRefs))
         const raceRecords:Object[] = []
+        const horseRecords:Object[] = []
         for (let horseRef of horseRefs) {
             console.log(`Fetching horse ${horseRef}`)
             await page.goto(`https://racing.hkjc.com${horseRef}&Option=1`) // "Show All" option
-            await page.waitForSelector('table[class="bigborder"]')
-            const headers = await page.$$eval('table[class="bigborder"] tr td[class="hsubheader"]', (elems => elems.map(elem => elem.textContent ?? '')))
-            const cells = await page.$$eval('table[class="bigborder"] tr td[class="htable_eng_text"]', (elems => elems.map(elem => elem.textContent ?? '')))
+            await page.waitForSelector('table[class="horseProfile"]')
+            const raceRecordHeaders = await page.$$eval('td[class="hsubheader"]', (elems => elems.map(elem => elem.textContent?.replace(/^\s+/, '').replace(/\s+$/, '') ?? '')))
+            const raceRecordCells = await page.$$eval('td[class="htable_eng_text"]', (elems => elems.map(elem => elem.textContent?.replace(/^\s+/, '').replace(/\s+$/, '') ?? '')))
             // TODO: parse and save horse info (e.g. sire, dam) => cross check what's available for future races
-            let raceRecord : { [key: string]: string } = {}
-            for (let i = 0; i < cells.length; ++i) {
-                raceRecord[headers[i % headers.length]] = cells[i].replace(/^\s+/, '').replace(/\s+$/, '')
-                if (i % headers.length === headers.length - 1) {
+            let raceRecord:{[key: string]: string} = {}
+            for (let i = 0; i < raceRecordCells.length; ++i) {
+                raceRecord[raceRecordHeaders[i % raceRecordHeaders.length]] = raceRecordCells[i]
+                if (i % raceRecordHeaders.length === raceRecordHeaders.length - 1) {
                     raceRecords.push(raceRecord)
                     raceRecord = {}
                 }
             }
+            const horseRecordCells = await page.$$eval('table[class="table_eng_text"] td', (elems => elems.map(elem => elem.textContent?.replace(/^\s+/, '').replace(/\s+$/, '') ?? ''))) 
+            let horseRecord:{[key:string]: string} = {}
+            for (let i = 2; i < horseRecordCells.length; i += 3) {
+                horseRecord[horseRecordCells[i - 2]] = horseRecordCells[i]
+            }
+            horseRecords.push(horseRecord)
             console.log(`Found ${raceRecords.length} race records`)
+            console.log(`Found ${horseRecords.length} horse records`)
             await sleep(500)
-            break // for debug
+            // break // for debug
         }
         await writeFile('./output/raceRecords.json', JSON.stringify(raceRecords))
+        await writeFile('./output/horseRecords.json', JSON.stringify(horseRecords))
     } catch (e) {
         console.error(e)
     } finally {
